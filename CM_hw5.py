a40391471d3a6b22eda9b422e908623d1c69ea98
Albert Wu <38256449+CrazyAncestor@users.noreply.github.com>
2024-09-30 12:28:54 -0500

    Add files via upload

--- CM_hw5.py
+++ CM_hw5.py
@@ -1,30 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
-A= 0.9
-beta = 0.1
-omega0 =20
-tau = 2*np.pi/omega0
-w = (omega0**2 - beta**2)**0.5
 
-def G(t,t0):
-    if t>t0:
-        return np.exp(-(t-t0))*np.sin(w*(t-t0))
-    else:
-        return 0
+global A_crit
 
-def all_G(t):
-    sum = 0
-    for n in range(10):
-        sum += G(t,n/2*tau) * A**n
-    return sum
+def displacement(A,make_plot=False):
+    beta = 1
+    omega0 =20
+    w = (omega0**2 - beta**2)**0.5
+    tau = 2*np.pi/w
 
-t = np.linspace(0,10,1000)
-x = []
-for i in range(len(t)):
-    #x.append(G(t[i],2.5))
-    x.append(all_G(t[i]))
 
-print(beta * tau)
-print(A**2*(1-A))
-plt.plot(t,x)
-plt.show()
+    def G(t,t0):
+        if t>t0:
+            return np.exp(-beta*(t-t0))*np.sin(w*(t-t0))
+        else:
+            return 0
+
+    def all_G(t):
+        sum = 0
+        for n in range(100):
+            sum += G(t,n/2*tau) * A**n
+        return sum
+
+    t = np.linspace(0,10,1000)
+    x = []
+    y = []
+
+    global A_crit
+    A_crit = np.exp(-beta*tau/2)
+    for i in range(len(t)):
+        x.append(all_G(t[i]))
+        y.append( np.exp(-beta*(t[i])))
+    
+    if make_plot:
+        plt.plot(t,x,label = 'x(t) for A='+str(A))
+        plt.plot(t,y,label = r'y(t)=$e^{-\beta t}$')
+        plt.xlabel('time')
+        plt.ylabel('displacement')
+        plt.legend()
+        plt.show()
+
+    return t,x,y
+
+
+t,x1,y = displacement(0.8,True)
+t,x2,y = displacement(0.85,True)
+t,x3,y = displacement(0.95,True)
